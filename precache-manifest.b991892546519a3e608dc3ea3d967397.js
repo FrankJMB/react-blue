9ea5c2ae12a8b8e444eb681c84791da8a824feaf
FrankJMB <frank.beentjes@live.nl>
2019-03-26 10:14:58 +0000

    Updates

---
+++ precache-manifest.b991892546519a3e608dc3ea3d967397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-blue/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c9dfafc2682c26579981",
+    "url": "/react-blue/static/js/runtime~main.ca2e1a3b.js"
+  },
+  {
+    "revision": "bc4d54d2d2b0a47e332b",
+    "url": "/react-blue/static/js/main.eef2cce4.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/react-blue/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "bc4d54d2d2b0a47e332b",
+    "url": "/react-blue/static/css/main.01534e8c.chunk.css"
+  },
+  {
+    "revision": "82fd26a46c2be7d7e9267bd5fd80270d",
+    "url": "/react-blue/index.html"
+  }
+];

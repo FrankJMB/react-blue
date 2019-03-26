bfd791d16c0fafaa8c104d5c83ffa17a05398474
FrankJMB <frank.beentjes@live.nl>
2019-03-26 12:19:12 +0100

    !!!

--- jenkins/scripts/install.sh
+++ jenkins/scripts/install.sh
@@ -8,6 +8,7 @@ npm install
 
 apk update
 apk add git
-git config --global user.name 'FrankJMB'
-git config --global user.email 'frank.beentjes@live.nl'
-git remote set-url origin https://FrankJMB:05meG1tHub@github.com/FrankJMB/react-blue.git
+
+# git config --global user.name 'FrankJMB'
+# git config --global user.email 'frank.beentjes@live.nl'
+# git remote set-url origin https://FrankJMB:05meG1tHub@github.com/FrankJMB/react-blue.git
